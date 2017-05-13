9a8b4cd89ac0f69f1ef85878769dbac05203a448
App Academy Student <student@appacademy.io>
2017-05-12 17:06:03 -0700

    Partial work on rendering sign-in / sign-up

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Actions from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
+import * as Actions from './actions/session_actions';
 
 window.signin = Actions.signin;
 window.signup = Actions.signup;
 window.signout = Actions.signout;
 
+
+
 document.addEventListener('DOMContentLoaded',() => {
-  window.store = configureStore();
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
